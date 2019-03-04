a41ac287a66ff4e66e0eefc3f8fc3465a824ecfa
unknown <halkyarda4@students.rowan.edu>
2019-03-04 16:18:59 -0500

    Added a main method to recursively add each token to the list.

--- BrumbyCodeChecker/src/tokenlister/Lister.java
+++ BrumbyCodeChecker/src/tokenlister/Lister.java
@@ -1,10 +1,22 @@
 package tokenlister;
 import java.util.*;
 import node.*;
+
+
+
 public class Lister {
+	Scanner scnr = new Scanner;
 	public List<Token> tokens = new ArrayList<Token>();
+
+	public static void main(String[] args){
+		while(scnr.hasNext())
+			AddtoList(scnr.next());
+	}
 	
-	public void AddToList(Token sablecctokens) {	tokens.add(sablecctokens);	} 
+	public void AddToList(Token sablecctokens) {
+		
+		tokens.add(sablecctokens);	
+	} 
 	
 	public List<Token> GetList() {	return tokens;	}
 	
