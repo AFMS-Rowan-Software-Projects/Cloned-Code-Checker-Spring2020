71a424b1017d57f51efe299cf303ae6864e0ccfe
unknown <halkyarda4@students.rowan.edu>
2019-03-13 16:45:36 -0400

    changed error to change string to reader, and added exception cartches.

--- BrumbyCodeChecker/src/tokenlister/Lister.java
+++ BrumbyCodeChecker/src/tokenlister/Lister.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.io.*;
 import node.*;
 import lexer.*;
-import java.nio.charset.*;
 
 
 
@@ -15,15 +14,23 @@ public class Lister {
 	}
 	
 	public ArrayList<Token> AddToList(String a) {
-		InputStream StrStream = new ByteArrayInputStream(a.getBytes(Charset.forName("UTF-8")));
+		//InputStream StrStream = new ByteArrayInputStream(a.getBytes(Charset.forName("UTF-8")));
+		
 		lex = new Lexer
 				(new PushbackReader
-				((Reader) StrStream, 1024)); //cant cast from inputstream to reader.
+				(new StringReader(a), 1024)); 
 		tokens = new ArrayList<Token>();
-		Token T = lex.next();
-		while (!(T instanceof EOF)) {
-			tokens.add(lex.next());
+		Token T;
+		try {
+			T = lex.next();
+			while (!(T instanceof EOF)) {
+				tokens.add(lex.next());
+			}
 		}
+		catch (LexerException le)
+		{ System.out.println ("Lexer Exception " + le); }
+		catch (IOException ioe)
+		{ System.out.println ("IO Exception " +ioe); }
 		return tokens;
 	} 
 	
