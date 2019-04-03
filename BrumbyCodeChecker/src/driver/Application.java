d482c7b64c91ea92d4098c1a27e17d314ff1d6ad
Unknown <wesleyaegaz@gmail.com>
2019-04-03 19:51:49 -0400

    Created application class and updated the grammar

--- BrumbyCodeChecker/src/driver/Application.java
+++ BrumbyCodeChecker/src/driver/Application.java
@@ -9,6 +9,19 @@ import parser.*;
 
 public class Application {
 	public static void main(String[] args) {
-		
+		ArrayList<String> directory_contents;
+		ArrayList<TokenizedMethod> methods;
+		String current_file;
+		for(int i = 0; i < args.length; i++) {
+			directory_contents = directoryplaceholder(args[i]);//ANDREW AND KEVIN- your code replaces this line
+			for(String file : directory_contents) {
+				current_file = CFilesReader.readFile(file);//Take filepath and load it into a String
+				//TODO find methods within file and save them as TokenizedMethods
+			}
+		}
+	}
+	
+	public static ArrayList<String> directoryplaceholder(String s){
+		return null;
 	}
 }
