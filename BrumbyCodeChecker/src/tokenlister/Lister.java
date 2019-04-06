44aad2704a22e720b49736825ff131de9d2e7bf1
Unknown <wesleyaegaz@gmail.com>
2019-04-06 12:58:04 -0400

    Deleted unused class and revised Lister correctly

--- BrumbyCodeChecker/src/tokenlister/Lister.java
+++ BrumbyCodeChecker/src/tokenlister/Lister.java
@@ -8,43 +8,9 @@ import lexer.*;
 
 public class Lister {
 	private Lexer lex;
-	private String fileLocation;
-	private ArrayList<Token> constructorTokens;
-	private int[] duplicateLocations;
-	private int numDuplicates;
 	public ArrayList<Token> tokens;
 
-	public Lister(){
-	}
 	
-	public Lister(String filePath, ArrayList<Token> token) {
-		fileLocation = filePath;
-		constructorTokens = token;
-		duplicateLocations = new int[3];	}
-
-public String getFileLocation() {
-	return fileLocation;
-}
-
-public int getNumDuplicates() {
-	return numDuplicates;
-}
-
-public int[] getDuplicateLocations() {
-	return duplicateLocations;
-}
-
-public void addDuplicateLocation(int indexOfDuplicate) {
-	if(numDuplicates == duplicateLocations.length) {
-		int[] tempArray = duplicateLocations;
-		duplicateLocations = new int[(numDuplicates+3)];
-		for(int i = 0; i < numDuplicates; i++) {
-			duplicateLocations[i] = tempArray[i];
-		}
-	}
-	duplicateLocations[numDuplicates] = indexOfDuplicate;
-	numDuplicates++;
-}
 
 public ArrayList<Token> AddToList(String a) {
 		//InputStream StrStream = new 	ByteArrayInputStream(a.getBytes(Charset.forName("UTF-8")));
