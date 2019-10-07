ec8dee00751779066304e5757129a6b2f978ee70
Collin Stilwell <cstilwell117@gmail.com>
2019-10-07 15:03:32 -0400

    Added Plugin Functionality

--- BrumbyCodeChecker/src/driver/Application.java
+++ BrumbyCodeChecker/src/driver/Application.java
@@ -11,23 +11,37 @@ import tokenlister.*;
 import brumby.*;
 
 /**
- * To run this program, right click Application.java 
- * Run As -- Run Configurations -- Arguments Tab
- * Copy and paste a pathname to a folder containing java, cpp, or ada files
- * After the pathname is copied, hit 'space' and type ".java", ".cpp", or ".ada"
- * Hit run.
+ * To run this program, right click Application.java Run As -- Run
+ * Configurations -- Arguments Tab Copy and paste a pathname to a folder
+ * containing java, cpp, or ada files After the pathname is copied, hit 'space'
+ * and type ".java", ".cpp", or ".ada" Hit run.
  */
+
 public class Application {
-	public static void main(String[] args) {
+
+	/**
+	 * Method to be called for Checker Plugin
+	 * 
+	 * @param path - file path for classes to be checked
+	 * 
+	 * @param type - what is the supported language: .java .cpp
+	 * 
+	 * Works similar to the application main method, but accepts the user inputted
+	 * information rather than the file.io input
+	 * 
+	 * @return a String containing all of the information that needs to be displayed
+	 * to the screen
+	 */
+	public String plugin(String path, String type) {
 		ArrayList<String> directory_contents;
 		ArrayList<TokenizedMethod> methods = new ArrayList<TokenizedMethod>();
 		ArrayList<Token> file_tokens, method_tokens;
-		
+
 		String current_file;
 		String qualified_name;
-		
+
 		int[] method_indices = new int[2];
-		
+
 		boolean isDuplicate;
 		double perc = 0.0;
 		int count = 0;
@@ -35,33 +49,35 @@ public class Application {
 		int filesAffected = 0;
 		ArrayList<String> filesWithDuplicates = new ArrayList<>();
 		Renamer rename;
-		
+
 		// Looks in the Arguments parameters in Run Configurations
-		for (int i = 0; i < args.length - 1; i++) {
-			// Searches and stores files in a directory with language extensions at the end of each file name
-			directory_contents = SearchFile.searchForFile(args[i], args[args.length - 1]);
+		String[] parts = path.split(" ");
+		for (String part : parts) {
+			// Searches and stores files in a directory with language extensions at the end
+			// of each file name
+			directory_contents = SearchFile.searchForFile(part, type);
 			for (String file : directory_contents) {
 				totalFiles++;
 				// Take file-path and store it into a String
 				current_file = CFilesReader.readFile(file);
-				// Clean up unused tokens in all files and store it into an ArrayList of "file_tokens"
-				file_tokens = Parser.sanitize(Lister.ConvertToList(current_file, args[args.length - 1]));
+				// Clean up unused tokens in all files and store it into an ArrayList of
+				// "file_tokens"
+				file_tokens = Parser.sanitize(Lister.ConvertToList(current_file, type));
 				method_indices[0] = 0;
 				while (method_indices[0] != -1) {
 					method_indices = Parser.findMethod(file_tokens, method_indices[1]);
-					if (method_indices[0] == -1) {	
-						break;	
-					} 
-					else {
+					if (method_indices[0] == -1) {
+						break;
+					} else {
 						// Remove method chunk and rename identifiers accordingly
 						rename = new Renamer(Parser.subarray(file_tokens, method_indices[0], method_indices[1]));
 						qualified_name = file + ":" + rename.getTokens().get(1).getText() + ":"
-											  + rename.getTokens().get(1).getLine();
+								+ rename.getTokens().get(1).getLine();
 						rename.parseFile();
 						method_tokens = rename.getTokens();
 						isDuplicate = false;
 						for (TokenizedMethod method : methods) {
-							// Get the similarity of 2 files or more. 
+							// Get the similarity of 2 files or more.
 							// If there's 3 files, 2 of which are 100% match, but the 3rd
 							// file is not 100% the same as the others, similarity will go down
 							// This similarity is for all files in a directory.
@@ -69,8 +85,7 @@ public class Application {
 							perc = perc + Parser.similarity(method.getTokens(), method_tokens);
 							count++;
 							// If both methods are exactly the same, (==1) then they are exact duplicates
-							if (Parser.similarity(method
-									.getTokens(), method_tokens) == 1) {
+							if (Parser.similarity(method.getTokens(), method_tokens) == 1) {
 								// Duplicate method found
 								isDuplicate = true;
 								// Record location of this duplicate, but throw away its tokenized
@@ -93,8 +108,121 @@ public class Application {
 		} // end of for(int i =...)
 		
 		// All directories processed and all methods added
+				int locAffected = 0;
+				perc = Math.round(perc / count * 100);
+				StringBuilder duplicatePaths = new StringBuilder();
+				for (TokenizedMethod method : methods) {
+					if (method.hasDuplicate()) {// TODO change to file output
+						/*
+						System.out.println("\nDuplicate found in the following files:");
+						System.out.println(method.toString());
+						*/
+						duplicatePaths.append("\nDuplicate found in the following files:\n")
+									  .append(method.toString())
+									  .append("\n");
+						locAffected += method.linesAffected();
+					}
+				}
+
+		// percent duplication that is in the files
+		String highOrLow;
+		if (perc < 79)
+			highOrLow = "Low Percent: ";
+		else {
+			highOrLow = "High Percent: ";
+		}
+
+		// built output for information window
+		String outputString = new StringBuilder().append(duplicatePaths.toString()).append("\n\n")
+				.append("Total Files: " + totalFiles + "\n").append("Affected Files: " + filesAffected + "\n")
+				.append("Lined of Code Affected: " + locAffected + "\n").append(highOrLow + perc + "%").toString();
+
+		// return the formatted information to be used by the plugin output window
+		return outputString;
+	}
+
+
+	/*
+	 * To be used when running the application as a normal java application
+	 * Takes in the file information via configuration and file io
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> directory_contents;
+		ArrayList<TokenizedMethod> methods = new ArrayList<TokenizedMethod>();
+		ArrayList<Token> file_tokens, method_tokens;
+
+		String current_file;
+		String qualified_name;
+
+		int[] method_indices = new int[2];
+
+		boolean isDuplicate;
+		double perc = 0.0;
+		int count = 0;
+		int totalFiles = 0;
+		int filesAffected = 0;
+		ArrayList<String> filesWithDuplicates = new ArrayList<>();
+		Renamer rename;
+
+		// Looks in the Arguments parameters in Run Configurations
+		for (int i = 0; i < args.length - 1; i++) {
+			// Searches and stores files in a directory with language extensions at the end
+			// of each file name
+			directory_contents = SearchFile.searchForFile(args[i], args[args.length - 1]);
+			for (String file : directory_contents) {
+				totalFiles++;
+				// Take file-path and store it into a String
+				current_file = CFilesReader.readFile(file);
+				// Clean up unused tokens in all files and store it into an ArrayList of
+				// "file_tokens"
+				file_tokens = Parser.sanitize(Lister.ConvertToList(current_file, args[args.length - 1]));
+				method_indices[0] = 0;
+				while (method_indices[0] != -1) {
+					method_indices = Parser.findMethod(file_tokens, method_indices[1]);
+					if (method_indices[0] == -1) {
+						break;
+					} else {
+						// Remove method chunk and rename identifiers accordingly
+						rename = new Renamer(Parser.subarray(file_tokens, method_indices[0], method_indices[1]));
+						qualified_name = file + ":" + rename.getTokens().get(1).getText() + ":"
+								+ rename.getTokens().get(1).getLine();
+						rename.parseFile();
+						method_tokens = rename.getTokens();
+						isDuplicate = false;
+						for (TokenizedMethod method : methods) {
+							// Get the similarity of 2 files or more.
+							// If there's 3 files, 2 of which are 100% match, but the 3rd
+							// file is not 100% the same as the others, similarity will go down
+							// This similarity is for all files in a directory.
+							// We may need to come back to change this
+							perc = perc + Parser.similarity(method.getTokens(), method_tokens);
+							count++;
+							// If both methods are exactly the same, (==1) then they are exact duplicates
+							if (Parser.similarity(method.getTokens(), method_tokens) == 1) {
+								// Duplicate method found
+								isDuplicate = true;
+								// Record location of this duplicate, but throw away its tokenized
+								// representation
+								method.addDuplicate(qualified_name);
+								// keeps track of files with duplicates
+								if (!filesWithDuplicates.contains(file)) {
+									filesWithDuplicates.add(file);
+									filesAffected++;
+								}
+								break;
+							}
+						} // end of for(TokenizedMethod...)
+						if (!isDuplicate) {
+							methods.add(new TokenizedMethod(qualified_name, method_tokens));
+						}
+					}
+				}
+			}
+		} // end of for(int i =...)
+
+		// All directories processed and all methods added
 		int locAffected = 0;
-		perc = Math.round(perc/count * 100);
+		perc = Math.round(perc / count * 100);
 		for (TokenizedMethod method : methods) {
 			if (method.hasDuplicate()) {// TODO change to file output
 				System.out.println("\nDuplicate found in the following files:");
@@ -105,12 +233,13 @@ public class Application {
 		System.out.println("Total Files: " + totalFiles);
 		System.out.println("Affected Files: " + filesAffected);
 		System.out.println("Lines of code affected: " + locAffected);
+
 		// Will need to change this later.
-		if(perc < 79)
+		if (perc < 79)
 			System.out.println("Low Percent: " + perc + "%");
 		else {
 			System.out.println("High Percent: " + perc + "%");
 		}
+
 	}
 }
-
