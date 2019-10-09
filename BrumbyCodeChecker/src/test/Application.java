ab0369611c7242eda942c05cf8cf1a06ae4b833d
Matthew Tam <tammat87@students.rowan.edu>
2019-10-09 11:00:53 -0400

    Updated Test Folder
    Added two files that should 100% flag the program.  They're just direct copies of each other.

---
+++ BrumbyCodeChecker/src/test/Application.java
@@ -0,0 +1,8 @@
+public class Application {
+
+public int Application() {
+int h = 5;
+h = h + 10;
+return h;
+}
+}
---
+++ BrumbyCodeChecker/src/test/fully - Copy.java
@@ -0,0 +1,8 @@
+public class fully {
+
+public int fully() {
+int h = 5;
+h = h + 1;
+return h;
+}
+}
